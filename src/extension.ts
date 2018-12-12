724d075b017a710efffec6ededc398b5fc3b0194
lilinhao <lilinhao@meituan.com>
2018-12-12 19:13:32 +0800

    feature: 添加点击文件高亮和循环引用检测

--- src/extension.ts
+++ src/extension.ts
@@ -12,9 +12,7 @@ import Pylon, {
   analyzeFile
 } from 'pylonn-llh';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
+import { getWebviewContent } from './util';
 // type Path = Pick<UserOption, 'dictionaryPath' | 'tsConfigPath' | 'statFile'>;
 interface UserOption extends IuserOpton {
   dictionaryPath: string;
@@ -34,7 +32,7 @@ const LANGUAGE_IDS = [
 
 const DEFAULT_OPTION = {
   dictionaryPath: './',
-  tsConfigPath: './',
+  tsConfigPath: './tsconfig.json',
   isJs: true,
   alias: {}
 };
@@ -42,9 +40,19 @@ const DEFAULT_OPTION = {
 function getOption() {
   if (rootPath) {
     let option: UserOption = DEFAULT_OPTION;
-    const optionFile = path.resolve(rootPath, './pylon.config.js');
-    if (fs.existsSync(optionFile)) {
-      option = require(optionFile);
+    const jsProjectConfig = path.resolve(rootPath, './config/default.js');
+    const jsonProjectConfig = path.resolve(rootPath, './config/default.json');
+    const optionFilePath = path.resolve(rootPath, './.depentrc.js');
+    if (fs.existsSync(jsProjectConfig)) {
+      let defaultConfig = require(jsProjectConfig);
+      option.alias = defaultConfig.alias;
+    }
+    if (fs.existsSync(jsonProjectConfig)) {
+      let defaultConfig = require(jsonProjectConfig);
+      option.alias = defaultConfig.alias;
+    }
+    if (fs.existsSync(optionFilePath)) {
+      option = require(optionFilePath);
     }
     option = {
       ...DEFAULT_OPTION,
@@ -68,6 +76,23 @@ function getOption() {
   return DEFAULT_OPTION;
 }
 
+// 生成gTree并写入缓存
+const genGtree = async (cacheFile: string) => {
+  const statusMessage = vscode.window.setStatusBarMessage(
+    '正在分析Dependency Tree...'
+  );
+  p = new Pylon(getOption());
+  setTimeout(async () => {
+    gTree = await p.genGTree();
+    // 生产缓存文件
+    fs.writeFile(cacheFile, JSON.stringify(gTree), () => {
+      gTree = JSON.parse(fs.readFileSync(cacheFile).toString());
+      vscode.window.showInformationMessage('Dependency Tree: 分析工作完成!');
+      statusMessage.dispose();
+    });
+  }, 500);
+};
+
 export function activate(context: vscode.ExtensionContext) {
   console.log('vscode-pylonn" is now active!');
   // 插件目录
@@ -78,40 +103,39 @@ export function activate(context: vscode.ExtensionContext) {
     '../',
     `${workSpanceName}-pylon.json`
   );
-  console.log(cacheFile);
-  if (fs.existsSync(cacheFile)) {
+  const isExistedCacheFile = fs.existsSync(cacheFile);
+  console.log('cacheFile', cacheFile);
+  console.log('isExistedCacheFile', isExistedCacheFile);
+  if (isExistedCacheFile) {
     try {
       gTree = JSON.parse(fs.readFileSync(cacheFile).toString());
     } catch (error) {
       fs.unlinkSync(cacheFile);
     }
+  } else {
+    genGtree(cacheFile);
   }
-  let init = vscode.commands.registerCommand('pylon.init', async () => {
-    vscode.window.showInformationMessage('Pylon init begin');
-    setTimeout(async () => {
-      p = new Pylon(getOption());
-      gTree = await p.genGTree();
-      // 生产缓存文件
-      fs.writeFile(cacheFile, JSON.stringify(gTree), () => {
-        gTree = JSON.parse(fs.readFileSync(cacheFile).toString());
-        vscode.window.showInformationMessage('Pylon init success');
-      });
-    }, 500);
+
+  let UpdateCache = vscode.commands.registerCommand('pylon.updateCache', () => {
+    genGtree(cacheFile);
   });
 
-  // let update = vscode.commands.registerCommand('pylon.updateGtree', () => {});
   let DependentTree = vscode.commands.registerCommand(
     'pylon.genDependentTree',
     param => {
       const fsPath = param.fsPath;
-      const dependentTree = genDepenceTree(fsPath, gTree);
+      const { dependentTree, circle } = genDepenceTree(fsPath, gTree);
       const panel = vscode.window.createWebviewPanel(
         'DpTree',
         'Dependent Tree',
         vscode.ViewColumn.One,
         { enableScripts: true }
       );
-      panel.webview.html = getWebviewContent(dependentTree, String(rootPath));
+      panel.webview.html = getWebviewContent(
+        dependentTree,
+        String(rootPath),
+        circle
+      );
     }
   );
 
@@ -119,14 +143,18 @@ export function activate(context: vscode.ExtensionContext) {
     'pylon.genBeDependentTree',
     param => {
       const fsPath = param.fsPath;
-      const beDependentTree = genBeDependentTree(fsPath, gTree);
+      const { beDependentTree, circle } = genBeDependentTree(fsPath, gTree);
       const panel = vscode.window.createWebviewPanel(
         'BeDpTree',
         'Bedependent Tree',
         vscode.ViewColumn.One,
         { enableScripts: true }
       );
-      panel.webview.html = getWebviewContent(beDependentTree, String(rootPath));
+      panel.webview.html = getWebviewContent(
+        beDependentTree,
+        String(rootPath),
+        circle
+      );
     }
   );
 
@@ -135,9 +163,9 @@ export function activate(context: vscode.ExtensionContext) {
       fs.unlink(cacheFile, error => {
         if (error) {
           console.log(error);
-          vscode.window.showErrorMessage('clear cache file failed!');
+          vscode.window.showErrorMessage('Dependency Tree: 清除缓存失败!');
         }
-        vscode.window.showInformationMessage('clear cache success!');
+        vscode.window.showInformationMessage('Dependency Tree: 清除缓存成功!');
       });
     }
   });
@@ -164,166 +192,13 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  context.subscriptions.push(DependentTree, BeDpendentTree, ClearCache, init);
+  context.subscriptions.push(
+    DependentTree,
+    BeDpendentTree,
+    ClearCache,
+    UpdateCache
+  );
 }
 
-function getWebviewContent(data: any, rootPath: string) {
-  return `<!DOCTYPE html> <meta charset="utf-8" />
-  <style>
-    html, body {
-      height: 100%;
-      width: 100%;
-      background: #fff;
-    }
-    .node circle {
-      cursor: pointer;
-      stroke: #3182bd;
-      stroke-width: 1.5px;
-    }
-  
-    .node text {
-      font: 10px sans-serif;
-      pointer-events: none;
-      text-anchor: middle;
-    }
-  
-    line.link {
-      fill: none;
-      stroke: #9ecae1;
-      stroke-width: 1.5px;
-    }
-  </style>
-  <body>
-    <script src="https://d3js.org/d3.v3.min.js"></script>
-    <script>
-      var width = 1000,
-          rootPath = ${JSON.stringify(rootPath + '/')},
-        height = 800,
-        root =${JSON.stringify(data)}
-  
-      var force = d3.layout
-        .force()
-        .linkDistance(100)
-        .charge(-320)
-        .gravity(0.08)
-        .size([width, height])
-        .on('tick', tick);
-  
-      var svg = d3
-        .select('body')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-  
-      var link = svg.selectAll('.link'),
-        node = svg.selectAll('.node');
-        update();
-      function update() {
-        var nodes = flatten(root),
-          links = d3.layout.tree().links(nodes);
-  
-        // Restart the force layout.
-        force
-          .nodes(nodes)
-          .links(links)
-          .start();
-  
-        // Update links.
-        link = link.data(links, function(d) {
-          return d.target.id;
-        });
-  
-        link.exit().remove();
-  
-        link
-          .enter()
-          .insert('line', '.node')
-          .attr('class', 'link');
-  
-        // Update nodes.
-        node = node.data(nodes, function(d) {
-          return d.id;
-        });
-  
-        node.exit().remove();
-  
-        var nodeEnter = node
-          .enter()
-          .append('g')
-          .attr('class', 'node')
-          .on('click', click)
-          .call(force.drag);
-  
-        nodeEnter.append('circle').attr('r',10);
-  
-        nodeEnter
-          .append('text')
-          .attr('dy', '.35em')
-          .text(function(d) {
-            return d.name.replace(rootPath, '');
-          });
-  
-        node.select('circle').style('fill', color);
-      }
-  
-      function tick() {
-        link
-          .attr('x1', function(d) {
-            return d.source.x;
-          })
-          .attr('y1', function(d) {
-            return d.source.y;
-          })
-          .attr('x2', function(d) {
-            return d.target.x;
-          })
-          .attr('y2', function(d) {
-            return d.target.y;
-          });
-  
-        node.attr('transform', function(d) {
-          return 'translate(' + d.x + ',' + d.y + ')';
-        });
-      }
-  
-      function color(d) {
-        return d._children
-          ? '#3182bd' // collapsed package
-          : d.children
-          ? '#c6dbef' // expanded package
-          : '#fd8d3c'; // leaf node
-      }
-  
-      // Toggle children on click.
-      function click(d) {
-        if (d3.event.defaultPrevented) return; // ignore drag
-        if (d.children) {
-          d._children = d.children;
-          d.children = null;
-        } else {
-          d.children = d._children;
-          d._children = null;
-        }
-        update();
-      }
-  
-      // Returns a list of all nodes under the root.
-      function flatten(root) {
-        var nodes = [],
-          i = 0;
-  
-        function recurse(node) {
-          if (node.children) node.children.forEach(recurse);
-          if (!node.id) node.id = ++i;
-          nodes.push(node);
-        }
-  
-        recurse(root);
-        return nodes;
-      }
-    </script>
-  </body>
-  `;
-}
 // this method is called when your extension is deactivated
 export function deactivate() {}
